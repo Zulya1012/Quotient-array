354b43cc3c4784b787d536f01a669621752e0766
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-25 15:06:51 +0400

    Update array4.java

--- array4.java
+++ array4.java
@@ -1,7 +1,15 @@
-public class array4{
-	public static void main(String[] arg){
-		int a[]={23, 62, 54, 8, 15, 30, 45};
-		int quotient=(a[1]-a[3])/a[2];
-		System.out.println(quotient);
-	}
+public class array4 {
+  public static void main(String[] arg) {
+    int a[] = {
+      23,
+      62,
+      54,
+      8,
+      15,
+      30,
+      45
+    };
+    int quotient = (a[1] - a[3]) / a[2];
+    System.out.println(quotient);
+  }
 }
